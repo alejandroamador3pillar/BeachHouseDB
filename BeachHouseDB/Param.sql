e40507ab2b08c0a5f0e42a02c2b349730c83e39e
Ale <aleamadorq@gmail.com>
2021-03-10 07:55:43 -0600

    Adding Params table

---
+++ BeachHouseDB/Param.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Param]
+(
+	[Id] INT NOT NULL , 
+    [description] VARCHAR(50) NOT NULL, 
+    [value] VARCHAR(50) NOT NULL, 
+    [start_date] DATETIME NULL, 
+    [end_date] DATETIME NULL, 
+    PRIMARY KEY ([Id])
+)
