64b5ff9dfbc9f3d1628282df6b4c02c76e8520b0
Ricardo <ricardo.bolanos@3pillarglobal.com>
2021-05-14 18:13:44 -0600

    update, insert table Seasons

--- BeachHouseDB/Reservations.sql
+++ BeachHouseDB/Reservations.sql
@@ -6,6 +6,9 @@
     [location_id] BIGINT NOT NULL, 
     [active] BIT NOT NULL, 
     [notified] BIT NULL, 
+    [created_by] VARCHAR(50) NULL, 
     CONSTRAINT [FK_Users_ToReservation] FOREIGN KEY ([user_id]) REFERENCES [Users]([Id]), 
+    CONSTRAINT [FK_Users_ToReservation2] FOREIGN KEY ([created_by]) REFERENCES [Users]([Id]), 
     CONSTRAINT [FK_Location_ToReservation] FOREIGN KEY ([location_id]) REFERENCES [Locations]([Id])
+
 )
---
+++ BeachHouseDB/Seasons.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Seasons
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [descriptionSeason] VARCHAR(50) NOT NULL, 
+	[typeseason] INT NOT NULL, 
+    [startdate] DATE NOT NULL, 
+    [enddate] DATE NOT NULL,
+    [ACTIVE] BIT NOT NULL,
+)
+
